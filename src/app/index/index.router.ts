f412b40c6e958a20402e149102c255f8882f14da
pullpushtree <bad7legend@gmail.com>
2021-01-21 19:47:39 -0500

    Implemented Welcome screen look

--- src/app/index/index.router.ts
+++ src/app/index/index.router.ts
@@ -9,22 +9,19 @@ const routes: Routes = [
     children: [
       {
         path: "",
-        loadChildren: () =>
-          import("../pages/welcome/welcome.module").then(
-            (m) => m.WelcomePageModule
-          ),
+        loadChildren: () => import("../pages/welcome/welcome.module").then((m) => m.WelcomePageModule),
       },
       {
         path: "login",
-        loadChildren: () =>
-          import("../pages/login/login.module").then((m) => m.LoginPageModule),
+        loadChildren: () => import("../pages/login/login.module").then((m) => m.LoginPageModule),
       },
       {
         path: "signup",
-        loadChildren: () =>
-          import("../pages/signup/signup.module").then(
-            (m) => m.SignupPageModule
-          ),
+        loadChildren: () => import("../pages/signup/signup.module").then((m) => m.SignupPageModule),
+      },
+      {
+        path: "welcome",
+        loadChildren: () => import("../pages/welcome/welcome.module").then((m) => m.WelcomePageModule),
       },
     ],
   },
--- src/app/pages/welcome/welcome.page.ts
+++ src/app/pages/welcome/welcome.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { IonSlides } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-welcome',
@@ -6,10 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome.page.scss'],
 })
 export class WelcomePage implements OnInit {
+  @ViewChild('slides', {static:true}) slides:IonSlides;
 
-  constructor() { }
+  slideOpts = {
+    initialSlide: 0,
+    effect: 'fade',
+    speed: 500
+
+  }
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  getStarted(){
+    this.slides.getActiveIndex().then(index => {
+      console.log(index);
+      this.slides.slideTo(3)
+    }) 
+   
+  }
+  loginAction(){
+    this.router.navigate(['login'])
+  }
+  signupAction(){
+    this.router.navigate(['signup'])
   }
 
 }
