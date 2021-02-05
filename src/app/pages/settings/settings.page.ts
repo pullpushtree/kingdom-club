c0585edf07812ee89024283968691a537a7e6557
pullpushtree <bad7legend@gmail.com>
2021-02-05 10:00:16 -0500

    Added dark light theme mode

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-settings',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsPage implements OnInit {
 
-  constructor() { }
+  constructor(private render: Renderer2,
+    private router: Router,
+    private afauth: AngularFireAuth
+    ) { }
 
   ngOnInit() {
   }
 
+  onToggleColorTheme(ev: any){
+    if(ev.detail.checked){
+      this.render.setAttribute(document.body, 'color-theme', 'dark')      
+    } else {
+      this.render.setAttribute(document.body, 'color-theme', 'light')      
+    }
+  }
+
+  logout(){
+    this.afauth.signOut()
+    .then(() => {
+      this.router.navigate(['/login']);
+    });
+  }
 }
