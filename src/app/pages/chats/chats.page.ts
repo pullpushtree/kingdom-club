24aa9a4cfa7177d46a4528c27a833b9fc084fcc8
pullpushtree <bad7legend@gmail.com>
2021-03-01 15:23:05 -0500

    updated chat page

--- src/app/pages/chats/chats.page.ts
+++ src/app/pages/chats/chats.page.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonContent } from '@ionic/angular';
+import { Observable } from 'rxjs';
+import { ChatService, Message } from 'src/app/services/chat.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-chats',
@@ -6,11 +11,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chats.page.scss'],
 })
 export class ChatsPage implements OnInit {
+  @ViewChild(IonContent) content: IonContent;
+
+  messages: Observable<Message[]>;
+  newMsg = '' ;
 
   constructor(
-    
+    private router: Router,
+    private chatService: ChatService
   ) { }
 
   ngOnInit() {
+    this.messages = this.chatService.getChatMessages();
+  }
+
+  sendMessage(){
+    this.chatService.addChatMessage(this.newMsg).then(()=> {
+      this.newMsg = '';
+      this.content.scrollToBottom();
+    })
+  }
+
+  goToContacts(){
+    this.router.navigate(['/home/contacts'])
   }
 }
