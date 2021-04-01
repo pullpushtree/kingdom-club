d237737b1bffaa52547a4f02aa10ec694314f885
pullpushtree <bad7legend@gmail.com>
2021-03-31 22:01:18 -0400

    added user's contact

--- src/app/pages/contacts/contacts.page.ts
+++ src/app/pages/contacts/contacts.page.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestore, fromDocRef } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
 import * as firebase from 'firebase/app';
+import { ChatService } from 'src/app/services/chat.service';
 
 @Component({
   selector: 'app-contacts',
@@ -17,46 +18,66 @@ export class ContactsPage implements OnInit {
   currentUser: any;
   image = "https://dummyimage.com/400";
   o_userId: string;
+  newChatId: string;
+  currentUserObject: Observable<any>;
 
   constructor(
     private router: Router,
     private afauthSrv: AuthService,   
     private afAuth: AngularFireAuth,
     private afs: AngularFirestore,
-  ) { }
+    private chatService: ChatService
+  ) { 
 
+  }
+  
   ngOnInit() {
     this.afauthSrv.user$.subscribe(user => {
-      this.currentUser = user;
-
-      console.log("this.currentUser.uid value", this.currentUser.uid)
-      this.getContactsFromFireBase()
+      this.currentUser = user;  
+      
+      this.currentUserObject = this.afs.collection("users").doc(this.currentUser.uid).valueChanges()
+     
+      this.loadCurrentUsersContacts();
     })
   }
 
-   async getContactsFromFireBase(){
-     this.contacts = this.afs
-       .collection(`contacts/${this.currentUser.uid}/contact`)  
-       .valueChanges();
-       console.log("Contacts: from getContactsFromFireBase ",this.contacts)
+   async loadCurrentUsersContacts(){
+    return this.contacts = this.chatService.getContactsFromFireBase();
    }
 
-//create new chat with selected contact
-  selectedContact(selCon: any){    
-    const selectContactUid = selCon.uid
-    console.log("this.contacts.ts selectedContact(selCon) selCon value : ",selectContactUid)
-    
-    this.o_userId = selCon.uid
-    const mesg = this.afs.collection(`messages`).add( {      
+  selectedContact(selectContactData: any){
+    this.o_userId = selectContactData.uid
+    this.afs.collection(`messages`).add( {      
       from: this.currentUser.uid,
       createdAt: firebase.default.firestore.FieldValue.serverTimestamp(),
-      participants: [this.currentUser.uid, this.o_userId]
+      participants: [
+        this.currentUser.uid,
+         this.o_userId,
+        ],
+      participantsDetails: [
+        {
+          displayName : this.currentUser.displayName,  
+          photoURL: this.currentUser.photoURL, 
+          uid: this.currentUser.uid
+        },
+        selectContactData
+      ]
+         
+         
+        
     }).then((docRef) => {
-      docRef.id
+      this.newChatId = docRef.id
       this.afs.doc(`messages/${docRef.id}`)
       .update({ msgId : docRef.id})
-      console.log("message res id is : ", docRef.id)
-    }).then(() => this.router.navigate(['/home/chats']))
+
+      localStorage.setItem('selectedConversationId', docRef.id ); 
+    }).then(() => 
+    this.router.navigate(['/home/chats', this.newChatId ]))
+  }
+
+  async startSelectedChat(selectedContact, i){    
+    return this.chatService.startNewChat(selectedContact, i)
+    
   }
 
 
