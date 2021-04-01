71f137bacdf50873c6e7fa3ed4a223567a408b3d
pullpushtree <bad7legend@gmail.com>
2021-03-31 21:49:02 -0400

    pckg json. default img for avatar, verbose routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,7 +20,8 @@ const routes: Routes = [
 
 @NgModule({
   imports: [    
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules, relativeLinkResolution: 'legacy' })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    //RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules, relativeLinkResolution: 'legacy' })
     //RouterModule.forRoot(routes, { enableTracing: true, preloadingStrategy: PreloadAllModules })
   ],
   exports: [RouterModule]
