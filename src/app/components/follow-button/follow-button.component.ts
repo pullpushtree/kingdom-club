ed2f87043afeb91ba794b83953bfbda65abee78e
pullpushtree <bad7legend@gmail.com>
2021-06-16 23:59:19 -0400

    Added follow button compoent

--- src/app/components/follow-button/follow-button.component.ts
+++ src/app/components/follow-button/follow-button.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FollowService } from "../../services/follow.service"
 import { size } from "lodash";
+import { SearchService } from 'src/app/services/search.service';
 
 @Component({
   selector: 'app-follow-button',
@@ -8,11 +9,13 @@ import { size } from "lodash";
   styleUrls: ['./follow-button.component.scss'],
 })
 export class FollowButtonComponent implements OnInit {
-  @Input() user;
-  @Input() currentUser;
+  @Input() otherUser: any;
+  @Input() otherUserId: string;
+  @Input() currentUser: any;  
+  @Input() isFollowing: boolean;
 
   followerCount: number;
-  isFollowing: boolean;
+  //isFollowing: boolean;
   image = "../../../assets/images/defaultProfile.jpg"
 
   followers: any;
@@ -20,25 +23,41 @@ export class FollowButtonComponent implements OnInit {
   followingCount: any;
 
   constructor(
-    private followSvc: FollowService
+    private followSvc: FollowService,
+    private searchSrvc: SearchService,
     ) { }
 
-  ngOnInit() {
+  ngOnInit() { }
 
-    const userId = this.user.uid
-    const currentUserId = this.currentUser.user.uid
+  changeFollow(){ 
+    this.followSvc.changeFollow(true) 
+  }
 
-    this.following = this.followSvc.getFollowing(currentUserId, userId)
-    .then(following => {      
-      this.isFollowing = following.exists
-      this.followingCount = this.countFollowing(following)
+  ionViewWillEnter(){
+    this.followSvc.currentFollowStatus.subscribe(followStatus => {
+      
+      this.isFollowing = followStatus 
     })
-
-    this.followers = this.followSvc.getFollowers(currentUserId)
-    .subscribe(followers => {
-     this.followerCount = this.countFollowers(followers)
+    
+      this.searchSrvc.getCurrentUser()
+    .subscribe((user) => {
+      this.currentUser = user;
+      const userId = this.otherUser.uid;
+      
+      this.following = this.followSvc.getFollowing(this.currentUser, userId)
+      .then(following => {
+        this.isFollowing = following.exists
+        this.followSvc.changeFollow(following.exists)        
+        this.followingCount = this.countFollowing(following)
+        console.log("COUNT VALUE FOLLOWING COUNT : ", this.followingCount)
+      })
+      
+      this.followers = this.followSvc.getFollowers()
+      .subscribe(followers => {
+        this.followerCount = this.countFollowers(followers)
+      })
     })
-  } 
+  }
 
   countFollowers(followers) {
     if(followers.$value === null) return 0
@@ -54,16 +73,15 @@ export class FollowButtonComponent implements OnInit {
     return size(following)
   }
 
-  toggleFollow(){
-    const userId = this.user.uid
-    const currentUserId = this.currentUser.user.uid
-
+  toggleFollow(oUserObj : any){
     if(this.isFollowing) {
-      this.followSvc.unfollow(currentUserId, userId)
-      this.isFollowing = false      
+      this.followSvc.unfollow(this.currentUser.uid, oUserObj.uid)
+      this.isFollowing = false  
+      this.followSvc.changeFollow(this.isFollowing)
     } else {
-      this.followSvc.follow(currentUserId, userId)
+      this.followSvc.follow(this.currentUser.uid, oUserObj.uid)
       this.isFollowing = true      
+      this.followSvc.changeFollow(this.isFollowing)
     }
   } 
 }
