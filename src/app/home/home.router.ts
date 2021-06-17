d3d717cb2853f1e0c34185855cb40de864dc6616
pullpushtree <bad7legend@gmail.com>
2021-06-16 23:59:58 -0400

    Added Chat, Contact, Message, Notification models

--- src/app/home/home.router.ts
+++ src/app/home/home.router.ts
@@ -16,7 +16,7 @@ const routes: Routes = [
         children: [
             {
                 path: '',
-                loadChildren:() => import('../pages/chats/chats.module').then(m => m.ChatsPageModule)
+                loadChildren: () => import('../pages/profile-gallery/profile-gallery.module').then( m => m.ProfileGalleryPageModule)
             },
             {
                 path: 'camera',
@@ -39,12 +39,16 @@ const routes: Routes = [
                 loadChildren: () => import('../pages/messages/messages.module').then( m => m.MessagesPageModule)
             },
             {
+                path: 'notifications',
+                loadChildren: () => import('../pages/notifications/notifications.module').then( m => m.NotificationsPageModule)
+            },
+            {
                 path: 'profile',
                 loadChildren:() => import('../pages/profile/profile.module').then(m => m.ProfilePageModule)
             },
             {
-              path: 'profile-comments/:id',
-              loadChildren: () => import('../pages/profile-comments/profile-comments.module').then( m => m.ProfileCommentsPageModule)
+                path: 'profile-comments/:id',
+                loadChildren: () => import('../pages/profile-comments/profile-comments.module').then( m => m.ProfileCommentsPageModule)
             },
             {
                 path: 'profile/edit',
@@ -59,7 +63,7 @@ const routes: Routes = [
                 loadChildren: () => import('../pages/profile-roll/profile-roll.module').then( m => m.ProfileRollPageModule)
             },
             {
-                path: 'profile-view',
+                path: 'profile-view/:id',
                 loadChildren: () => import('../pages/profile-view/profile-view.module').then( m => m.ProfileViewPageModule)
             },
             {
@@ -79,12 +83,12 @@ const routes: Routes = [
                 loadChildren: () => import('../pages/set-profile-images/set-profile-images.module').then( m => m.SetProfileImagesPageModule),    
             },
             {
-                path: '', redirectTo: 'profile', pathMatch: 'full'
+                path: '**', redirectTo: 'profile-gallery', pathMatch: 'full'
             }
         ]
     },
     {
-        path: '', redirectTo: 'home/profile', pathMatch: 'full'
+        path: '**', redirectTo: 'home/profile-gallery', pathMatch: 'full'
     }
 ];
 
